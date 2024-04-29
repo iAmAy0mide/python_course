f22ddf66a818cf8a3fba44dd5c9881dd443ff1f3
iAmAyomide <ayomidekenny952@gmail.com>
2024-04-29 06:16:07 -0700

    str mtds

--- str_mtds.py
+++ str_mtds.py
@@ -1,7 +1,14 @@
+# str.startswith(prefix) and str.endswith(suffix)
+str1 = "alphabet"
+print(str1.startswith(b))
+
+
 # str.strip(), str.lstrip(), and str.rstrip()
 
+"""
 sentence =  "      Python is fun       "
 print(sentence)
 print(sentence.strip())
 print(sentence.lstrip())
 print(sentence.rstrip())
+"""
