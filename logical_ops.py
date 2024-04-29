4597ec8ea6ddfdb9522c8474731a4387aabf2a24
iAmAyomide <ayomidekenny952@gmail.com>
2024-04-29 08:12:13 -0700

    logical ops

---
+++ logical_ops.py
@@ -0,0 +1,16 @@
+x = True 
+y = False
+
+#  not operator
+print(not y)
+
+
+# or | operator
+# print(True or False)
+
+
+#  and / & operator
+# print(True and False)
+# print(True & False)
+result = x and y
+print(result)
