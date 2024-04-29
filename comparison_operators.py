798b46c8bfcbac87da38d49452e5fafb9e13f00b
iAmAyomide <ayomidekenny952@gmail.com>
2024-04-29 07:10:19 -0700

    comparison_operators

---
+++ comparison_operators.py
@@ -0,0 +1,22 @@
+a = 3
+b = 7
+
+# <= operator
+print( a <= b)
+
+# >= operator
+print( a >= b)
+
+
+# < operator
+# print( a < b)
+
+# > operator
+print( a > b)
+
+# != operator
+# print(a != b)
+
+
+# == operator
+# print(a == b)
