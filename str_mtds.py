63ad7ec1a6bffc111c37b7337d6c28dfdfed9b90
iAmAyomide <ayomidekenny952@gmail.com>
2024-04-29 06:57:15 -0700

    str mtds

--- str_mtds.py
+++ str_mtds.py
@@ -1,6 +1,40 @@
+# str.count(substring)
+sentence = "Python is easy. Python is fun. Python is powerful."
+print(sentence.count("Python")) 
+
+
+
+# str.split(separator)
+"""
+str1 = "I Love Programming"
+print(str1.split(" "))
+"""
+
+
+
+# str.find(substring) and str.index(substring)
+"""
+str1 = "I Love Programming"
+print(str1.find("V"))
+print(str1.index("L"))
+"""
+
+
+
+# str.replace(old, new) || str.replace(searchValue, replaceValue)
+
+"""
+str1 = "I Like programming in Java 😊"
+print(str1.replace("Java", "Python"))
+"""
+
+
 # str.startswith(prefix) and str.endswith(suffix)
+"""
 str1 = "alphabet"
-print(str1.startswith(b))
+print(str1.startswith("b"))
+print(str1.endswith("t"))
+"""
 
 
 # str.strip(), str.lstrip(), and str.rstrip()
