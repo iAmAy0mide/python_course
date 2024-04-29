4bf8166a1f6ca86b9daa788217c587599cfbff75
iAmAyomide <ayomidekenny952@gmail.com>
2024-04-29 10:27:18 -0700

    lists

---
+++ Lists.py
@@ -0,0 +1,11 @@
+#  Creating a list of numbers
+num_list = [1, 2, 3, 4, 5]
+print(len(num_list))
+
+#  Creating a list of strings
+strList = ["apple", "orange", "fru"]
+print(strList)
+
+#  Mixed-type List
+mixed = ["we", 2, True, None]
+print(mixed)
