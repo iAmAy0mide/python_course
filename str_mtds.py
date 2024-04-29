03ce027e2a5cc34e9f7872ad21227f02ce31f4ea
iAmAyomide <ayomidekenny952@gmail.com>
2024-04-29 06:13:40 -0700

    str mtds

---
+++ str_mtds.py
@@ -0,0 +1,7 @@
+# str.strip(), str.lstrip(), and str.rstrip()
+
+sentence =  "      Python is fun       "
+print(sentence)
+print(sentence.strip())
+print(sentence.lstrip())
+print(sentence.rstrip())
