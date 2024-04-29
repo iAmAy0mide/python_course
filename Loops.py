a9a5a07f880f5d7f5c9eeebd6d878bfa8297bff8
iAmAyomide <ayomidekenny952@gmail.com>
2024-04-29 10:18:43 -0700

    loops

---
+++ Loops.py
@@ -0,0 +1,52 @@
+# Menu-Drive Program
+choice = None
+
+while choice != 'q':
+    print("1. Option 1")
+    print("2. Option 2")
+    print("3. Option 3")
+    choice = input("Enter your choice (or 'q' to quit)")
+
+
+# while loop
+"""
+password = "LongPassword"
+count = 0
+
+while count < 3: 
+    for char in password:
+        print(password.index(char))
+    print("Done")
+    count += 1
+"""
+
+
+# while len(password) >= 8: 
+#     for char in password:
+#         print(password.index(char))
+
+
+# Ex
+"""
+word = "Python"
+for i in word:
+    print(i.upper())
+""" 
+
+
+## range(start, stop, step)
+"""
+for i in range(1, 10, 5):
+    print(i)
+"""
+
+
+#Basics
+
+"""
+for i in range(5): 
+    print("We\'re good, too good!" )
+
+print("abd" + 3)
+"""
+
