3504c8a373b4acc76e624c170bf3b7817f0a56cb
iAmAyomide <ayomidekenny952@gmail.com>
2024-05-05 08:00:24 -0700

    s

---
+++ list_mtds.py
@@ -0,0 +1,33 @@
+# Removing / Deleting Items
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+print(my_list[2:9:3])
+
+
+
+# Updating Items
+"""
+my_list = [1, 2, 3, 4, 5, 6, 7]
+my_list[3] = 30
+print(my_list)
+"""
+
+
+"""
+slices [start:end:step]
+"""
+
+# Using slices
+"""
+my_list = [1, 2, 3, 4, 5]
+subset = my_list[1:3]
+print(subset)
+"""
+
+
+
+# Accessing Items
+"""
+my_list = [1, 2, 3, 4, 5]
+print(my_list[-1])
+"""
