6aed18ec09031a30554be6422789e4ef98e586af
iAmAyomide <ayomidekenny952@gmail.com>
2024-05-05 09:05:33 -0700

    list built in methods

---
+++ list_binmtds.py
@@ -0,0 +1,50 @@
+# copy 
+original_list = [1, 2, 3]
+copied_list = original_list
+print(copied_list)
+
+
+# sort 
+"""
+my_list = [1,2,3,9, 5, 6, 4, 87, 2, 6]
+my_list.sort()
+print(my_list)
+"""
+
+
+# pop - remove last item if index is not specified
+"""
+my_list = [1,2,3, "abc"]
+print(my_list.pop())
+"""
+
+
+# remove
+"""
+my_list = [1,2,3, "abc"]
+my_list.remove("abc")
+print(my_list)
+"""
+
+
+# insert
+"""
+my_list = [1,2,3]
+my_list.insert(1, 3)
+print(my_list)
+"""
+
+# extend - add to a list by iterable
+"""
+my_list = [1,2,3]
+my_list.extend([2,3,3,2])
+print(my_list)
+"""
+
+
+# append - add item to the list
+"""
+my_list = [1,2,3]
+my_list.append(4)
+print(my_list)
+"""
