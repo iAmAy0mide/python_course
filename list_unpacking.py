2bc70a01015e287ac64b1f34744d739d51e3ed23
iAmAyomide <ayomidekenny952@gmail.com>
2024-05-05 09:50:45 -0700

    list_unpacking

---
+++ list_unpacking.py
@@ -0,0 +1,14 @@
+# List unpacking
+my_list = [1, 2, 3, 4, 5, 6]
+
+first, second, *rest = my_list
+print(first, second)
+print(rest)
+
+
+# Basic Unpacking
+"""
+my_list = [1, 2, 3]
+a, b, c = my_list
+print(a, b, c)
+"""
