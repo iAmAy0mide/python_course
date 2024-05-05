e769c9e7096681afc9a52a7023f32712b80dcb40
iAmAyomide <ayomidekenny952@gmail.com>
2024-05-05 08:32:46 -0700

    list types

--- list_mtds.py
+++ list_mtds.py
@@ -1,7 +1,23 @@
 # Removing / Deleting Items
+
+# Using remove() 
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+my_list.reverse()
+my_list.remove(4)
+my_list.remove(10)
+print(my_list)
+
+
+# Using del
+"""
 my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
+del my_list[2:9:3]
+print(my_list)
 print(my_list[2:9:3])
+"""
+
+
 
 
 
