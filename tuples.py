474d8bbcad28b9f69a27c3492b40b461321551c4
iAmAyomide <ayomidekenny952@gmail.com>
2024-05-12 04:42:51 -0700

    tuple

---
+++ tuples.py
@@ -0,0 +1,12 @@
+my_tuple = (1, 2, 3, "hello", 3.34)
+
+# Creating a new tuple with modified content
+
+new_tuple = my_tuple + (5, "World")
+print(new_tuple)
+
+# my_tuple[3] = 4
+
+# print(my_tuple)
+# print(my_tuple[3])
+# print(type(my_tuple))
---
+++ w'
@@ -0,0 +1,20 @@
+l
+sldv';skvs
+'sk
+s
+c'
+c
+w
+w'k'wk
+w'l
+clw;kc
+'w
+cw'c.Wc
+W"l v;]
+ w;l cw
+wk c'wk
+]w
+c k'  c
+l c;lck 
+l
+w wl' w
