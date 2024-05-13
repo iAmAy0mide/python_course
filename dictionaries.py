b1457a8bc090ba741293e6427d3abb21ee6585be
iAmAyomide <ayomidekenny952@gmail.com>
2024-05-13 05:19:35 -0700

    dictionaries

---
+++ dictionaries.py
@@ -0,0 +1,30 @@
+# Using a List of Tuples
+tuple_list = [("name", "Ewa"), ("age", 28), ("city", "Berlin")]
+from_tuples_to_dict = dict(tuple_list)
+# print(from_tuples_to_dict)
+
+# Nested Dictionary
+nested_dict = {
+    "person": {"name": "Daniel", "age": 45},
+    "country": "UAE"
+}
+
+# print(nested_dict)
+
+
+# Dictionary with different Data Types
+mixed_dict = {"name": "Chrlie", "grades": [23, 44, 89, 90, 100, 500], "is_student": True}
+# print(mixed_dict)
+
+#  Using the dict() constructor
+data = dict(name="BobZ", age=18, career="programming")
+print(data)
+
+# Dict key-value
+s_info = {"name": "Dan", "age": 20, "grade": "A"}
+# print(s_info)
+
+
+# Empty Dictionary
+empty_dict = {}
+# print(type(empty_dict))
